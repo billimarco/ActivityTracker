0f821711c7135d11a53e6882dede4b302fb16ed8
maruko <marcolbilli@gmail.com>
2021-07-14 11:01:02 +0200

    Implementazione classe Activity

--- Activity.cpp
+++ Activity.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "Activity.h"
+
+Activity::Activity(std::string description, Time startTime, Time endTime) {
+    this->description = description;
+    if (startTime < endTime) {
+        this->startTime = startTime;
+        this->endTime = endTime;
+    }
+}
+
+const std::string &Activity::getDescription() const {
+    return description;
+}
+
+void Activity::setDescription(const std::string &description) {
+    Activity::description = description;
+}
+
+const Time &Activity::getStartTime() const {
+    return startTime;
+}
+
+void Activity::setStartTime(const Time &startTime) {
+    Activity::startTime = startTime;
+}
+
+const Time &Activity::getEndTime() const {
+    return endTime;
+}
+
+void Activity::setEndTime(const Time &endTime) {
+    Activity::endTime = endTime;
+}
--- Activity.h
+++ Activity.h
@@ -6,8 +6,32 @@
 #define ACTIVITYTRACKER_ACTIVITY_H
 
 
-class Activity {
+#include <string>
+#include "Date.h"
+#include "Time.h"
 
+class Activity {
+public:
+    Activity(std::string description, Time startTime, Time endTime);
+
+    ~Activity() = default;
+
+    const std::string &getDescription() const;
+
+    void setDescription(const std::string &description);
+
+    const Time &getStartTime() const;
+
+    void setStartTime(const Time &startTime);
+
+    const Time &getEndTime() const;
+
+    void setEndTime(const Time &endTime);
+
+private:
+    std::string description;
+    Time startTime{Time(0, 0)};
+    Time endTime{Time(0, 0)};
 };
 
 
